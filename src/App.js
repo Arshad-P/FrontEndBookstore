f568c9afb18ea760bb6b1461da11e364f16fb6fd
Arshad-P <arshadpatan365@gmail.com>
2023-11-16 21:03:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 
 import {BrowserRouter as Router,Routes,Route}from "react-router-dom"
-import './App.css';
-
 import Navbar from './components/Navbar';
 import Footer from "./components/Footer";
 import AddBooks from './pages/AddBooks';
